a5f6d0b1ddd22fb702a48c532d6fdc32d0c324f4
Jan Alonzo <jmalonzo@taguchimail.com>
2014-02-04 20:33:34 +1100

    Update to conform to the latest GitHub UI

--- src/content_script.js
+++ src/content_script.js
@@ -2,7 +2,7 @@
   var comments = $(".comment-body");
   comments.each(function(i, v) {
     if (v.children.length == 1 && $(v.children[0]).text().length < 3) {
-      $(v.children[0]).parents(".discussion-bubble").hide();      
+      $(v.children[0]).parents(".timeline-comment-wrapper").hide();
     }
   });
 })();
